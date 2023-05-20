7bc96045223c09f82132f8c4df8114ac8b779cd8
Yes-and <vojtawilson@gmail.com>
2023-05-20 22:16:42 +0100

    Update crossovers.py
    Remove one_point crossover (redundant), patch order crossover, completely rework cycle crossover

--- operators/crossovers.py
+++ operators/crossovers.py
@@ -1,24 +1,13 @@
 import random
 
-def one_point_crossover(p1, p2):
-
-    crossover_point = random.randint(1, len(p1))
-
-    o1 = p1[crossover_point:] + p2[:crossover_point]
-    o2 = p2[crossover_point:] + p1[:crossover_point]
-
-
-    return o1, o2
-
 def order_crossover(p1, p2):
     # select a set of elements from one parent, 
     # keep them, and fill the spaces with elements from another parent
     # while preserving their respective order
     how_many = 2
     parent_length = len(p1)
-    crossover_index = random.randint(0, parent_length-how_many+1)
-    # print(crossover_index)
-    
+    crossover_index = random.randint(0, parent_length-how_many)
+
     s1 = [p1[i] for i in range(crossover_index, crossover_index+how_many)]
     s2 = [p2[i] for i in range(crossover_index, crossover_index+how_many)]
     # print(s1, s2)
@@ -33,28 +22,25 @@ def order_crossover(p1, p2):
     return o1, o2
 
 def cycle_crossover(p1, p2):
-    # select values from random positions from one parent
-    # and fill the blanks with values from other parent
-    # while preserving their respective order
-    parent_length = len(p1)
-    how_many = random.randint(1, parent_length-1)
-    chosen_indexes = sorted(random.choices(population=[i for i in range(parent_length)], k=how_many))
-    # print(how_many, chosen_indexes)
+    # find a cycle and fill in the rest
+    cycle = [False]*len(p1)
 
-    s1 = [p1[i] for i in chosen_indexes]
-    s2 = [p2[i] for i in chosen_indexes]
-
-    u1 = [val for val in p2 if val not in s1]
-    u2 = [val for val in p1 if val not in s2]
+    index = 0
+    start = p1[index]
+    curr = p2[index]
+    while True:
+        cycle[index] = True
+        if curr==start:
+            break
+        index = p1.index(curr)
+        curr = p2[index]
 
     o1, o2 = [], []
-    for i in range(parent_length):
-        if i in chosen_indexes:
-            o1 += [s1.pop(0)]
-            o2 += [s2.pop(0)]
+    for i in range(len(p1)):
+        if cycle[i]==True:
+            o1.append(p1[i]), o2.append(p2[i])
         else:
-            o1 += [u1.pop(0)]
-            o2 += [u2.pop(0)]
+            o1.append(p2[i]), o2.append(p1[i])
 
     return o1, o2
 
@@ -76,7 +62,7 @@ def different_beginning_crossover(p1, p2):
 
     return o1, o2
 
-p1, p2 = ["A", "B", "C", "D", "E", "F", "G", "H"], ["G", "F", "E", "D", "C", "B", "A", "H"]
-o1, o2 = different_beginning_crossover(p1, p2)
+p1, p2 = ["A", "B", "D", "C", "E", "F", "G", "H"], ["G", "C", "E", "D", "B", "A", "F", "H"]
+o1, o2 = cycle_crossover(p1, p2)
 print(p1, p2)
 print(o1, o2)
