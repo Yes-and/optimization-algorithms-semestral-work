7d83fdd8299290bb34bec13a94cb9d16f2b68a15
Pedro Bonifácio <20211652@novaims.unl.pt>
2023-05-21 17:02:02 +0100

    selectors only do minimization now

--- operators/selectors.py
+++ operators/selectors.py
@@ -1,27 +1,21 @@
 import random
 
 
-def roulette_selection(population, fitnesses, maximization):
+def roulette_selection(population, fitnesses):
     sum_of_fitnesses = sum(fitnesses)
 
-    if maximization:
-        probabilities = [fitness / sum_of_fitnesses for fitness in fitnesses]
-    else:
-        probabilities = [1 - fitness / sum_of_fitnesses for fitness in fitnesses]
+    probabilities = [1 - fitness / sum_of_fitnesses for fitness in fitnesses]
 
     return random.choices(population, weights=probabilities)[0]
 
 
-def tournament_selection(population, fitnesses, k, maximization):
+def tournament_selection(population, fitnesses, k):
     # Part 1:
     p1_indexes = random.choices(range(len(population)), k=k)
     p1_population = [population[i] for i in p1_indexes]
     p1_fitnesses = [fitnesses[i] for i in p1_indexes]
 
     # Part 2:
-    if maximization:
-        winner_index = max(range(len(p1_fitnesses)), key=p1_fitnesses.__getitem__)
-    else:
-        winner_index = min(range(len(p1_fitnesses)), key=p1_fitnesses.__getitem__)
+    winner_index = min(range(len(p1_fitnesses)), key=p1_fitnesses.__getitem__)
 
     return p1_population[winner_index]
