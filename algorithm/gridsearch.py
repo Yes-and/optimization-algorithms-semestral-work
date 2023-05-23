3df105f5937b4a99afa207898f34215962974d1b
Pedro Bonifácio <20211652@novaims.unl.pt>
2023-05-23 17:32:11 +0100

    updated gridsearch.py

--- algorithm/gridsearch.py
+++ algorithm/gridsearch.py
@@ -12,7 +12,7 @@ param_grid = {
     "create_population": [create_population],
     "loss_matrix": [loss_matrix],
     "evaluate_population": [evaluate_population],
-    "gens": [50],
+    "gens": [250],
     "pop_size": [10],
     "selector": [roulette_selection, tournament_selection],
     "mutator": [swap_mutation, reverse_sequence_mutation, partial_shuffle_mutation, twors_mutation],
@@ -30,17 +30,19 @@ total_comb = sum(1 for _ in product(*param_grid.values()))
 lst_params, lst_fit = [], []
 
 for iteration in range(10):
-    best_fit, best_params = float("+inf"), None
+    best_mean_fit, best_min_fit, best_params = float("+inf"), float('+inf'), None
     random.seed()
     param_grid["loss_matrix"] = [generate_problem()]
     for count, params in enumerate(product(*param_grid.values())):
         print("Iterating... {}/{}".format(count, total_comb), end="\r")
         pop, fit_pop = GA(*params)
         mean_fit = round((sum(fit_pop) / len(fit_pop)), 2)
-        if mean_fit < best_fit:
-            best_fit = mean_fit
+        if mean_fit < best_mean_fit:
+            best_min_fit = round(min(fit_pop), 2)
+            best_mean_fit = mean_fit
             best_params = params[5:11]
-    print(best_params)
-    print(best_fit)
+    print("Parameters: ", best_params)
+    print("Mean fitness: ", best_mean_fit)
+    print("Min fitness: ", best_min_fit)
     lst_params.append(best_params)
-    lst_fit.append(best_fit)
+    lst_fit.append(best_mean_fit)
