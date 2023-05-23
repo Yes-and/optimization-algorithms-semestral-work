6e0d3e1ec7dfd3482380feae111dbaa05909f99e
Pedro Bonifácio <20211652@novaims.unl.pt>
2023-05-23 16:09:04 +0100

    updated gridsearch.py

--- algorithm/gridsearch.py
+++ algorithm/gridsearch.py
@@ -12,11 +12,10 @@ param_grid = {
     "create_population": [create_population],
     "loss_matrix": [loss_matrix],
     "evaluate_population": [evaluate_population],
-    "gens": [100],
+    "gens": [50],
     "pop_size": [10],
     "selector": [roulette_selection, tournament_selection],
-    "mutator": [swap_mutation, reverse_sequence_mutation, partial_shuffle_mutation, twors_mutation,
-                centre_inverse_mutation, throas_mutation, modification_thrors_mutation],
+    "mutator": [swap_mutation, reverse_sequence_mutation, partial_shuffle_mutation, twors_mutation],
     "crossover_operator": [order_crossover, cycle_crossover, partially_mapped_crossover],
     "p_c": [0.2, 0.4, 0.6, 0.8],
     "p_m": [0.1, 0.2, 0.3, 0.5],
@@ -28,15 +27,20 @@ param_grid = {
 
 
 total_comb = sum(1 for _ in product(*param_grid.values()))
-best_fit = float("+inf")
+lst_params, lst_fit = [], []
 
-for count, params in enumerate(product(*param_grid.values())):
-    print("Iterating... {}/{}".format(count, total_comb), end="\r")
-    pop, fit_pop = GA(*params)
-    mean_fit = round((sum(fit_pop) / len(fit_pop)), 2)
-    if mean_fit < best_fit:
-        best_fit = mean_fit
-        best_params = params[5:11]
-
-print(best_params)
-print(best_fit)
+for iteration in range(10):
+    best_fit, best_params = float("+inf"), None
+    random.seed()
+    param_grid["loss_matrix"] = [generate_problem()]
+    for count, params in enumerate(product(*param_grid.values())):
+        print("Iterating... {}/{}".format(count, total_comb), end="\r")
+        pop, fit_pop = GA(*params)
+        mean_fit = round((sum(fit_pop) / len(fit_pop)), 2)
+        if mean_fit < best_fit:
+            best_fit = mean_fit
+            best_params = params[5:11]
+    print(best_params)
+    print(best_fit)
+    lst_params.append(best_params)
+    lst_fit.append(best_fit)
