8c8193bb226347afec49b819b2e4f51b02636681
Pedro Bonifácio <20211652@novaims.unl.pt>
2023-05-23 15:50:41 +0100

    updated gridsearch.py

--- algorithm/gridsearch.py
+++ algorithm/gridsearch.py
@@ -12,25 +12,28 @@ param_grid = {
     "create_population": [create_population],
     "loss_matrix": [loss_matrix],
     "evaluate_population": [evaluate_population],
-    "gens": [1000],
-    "pop_size": [20],
+    "gens": [100],
+    "pop_size": [10],
     "selector": [roulette_selection, tournament_selection],
     "mutator": [swap_mutation, reverse_sequence_mutation, partial_shuffle_mutation, twors_mutation,
                 centre_inverse_mutation, throas_mutation, modification_thrors_mutation],
     "crossover_operator": [order_crossover, cycle_crossover, partially_mapped_crossover],
-    "p_c": [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
-    "p_m": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6],
+    "p_c": [0.2, 0.4, 0.6, 0.8],
+    "p_m": [0.1, 0.2, 0.3, 0.5],
     "elitism": [True, False],
     "verbose": [False],
     "log": [False],
     "path": ['log/test_log.csv'],
 }
 
+
+total_comb = sum(1 for _ in product(*param_grid.values()))
 best_fit = float("+inf")
-for params in product(*param_grid.values()):
+
+for count, params in enumerate(product(*param_grid.values())):
+    print("Iterating... {}/{}".format(count, total_comb), end="\r")
     pop, fit_pop = GA(*params)
     mean_fit = round((sum(fit_pop) / len(fit_pop)), 2)
-    print(mean_fit)
     if mean_fit < best_fit:
         best_fit = mean_fit
         best_params = params[5:11]
